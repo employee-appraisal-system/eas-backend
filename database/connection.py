dd0569813818bff951625fcba2dcf92ace3cd09d
Aaron Mathew <149572843+aaronwppe@users.noreply.github.com>
2026-05-20 15:53:32 +0530

    fix: remove BOM from database_backup.sql

--- database/connection.py
+++ database/connection.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
-# DATABASE_URL = "postgresql://postgres:a@localhost/Employee_Appraisal"
 
 # Load environment variables from .env
 load_dotenv()
